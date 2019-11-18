d0f62637210be9809b59aab5bec5f6fb63bf3347
AndreiDetenkov <a.detenkov@gmail.com>
2019-11-18 18:11:22 +0600

    #api prettier, express, cors
    - added prettier lib to api;
    - added .prettierrc file with settings for prettier;
    - plugged in express js library;
    - plugged in cors for express;
    - added listener to 8000 port;

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 8000;
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log('test app listening on port 8000!');
+});
