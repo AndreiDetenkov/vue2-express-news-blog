0a9debb6a8041d6996f53a84851013d943bc5302
AndreiDetenkov <a.detenkov@gmail.com>
2019-11-22 01:03:07 +0600

    # category schema
    - added mongoose category schema to models;
    - added fields title, parentId;

---
+++ api/models/category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  parentId: {
+    type: Schema.Types.ObjectId,
+    default: null,
+  },
+});
+
+const Category = mongoose.model('Category', CategorySchema);
+module.exports = Category;
