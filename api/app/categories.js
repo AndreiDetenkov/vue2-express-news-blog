c5c14f749250a4e69a9948c11e35594b20ed4a61
AndreiDetenkov <a.detenkov@gmail.com>
2019-11-23 14:54:27 +0600

    # app categories fixed code
    - added new route of request parent categories;
    - added populate(title) to categories list request;

--- api/app/categories.js
+++ api/app/categories.js
@@ -2,39 +2,49 @@ const express = require('express');
 const Category = require('../models/category');
 
 const createRouter = () => {
-	const router = express.Router();
+  const router = express.Router();
 
-	router.get('/list', async (req, res) => {
-		try {
-			const categories = await Category.find();
-			if (categories) {
-				res.send(categories);
-			}
-		} catch (error) {
-			return res.status(500).send({message: 'There are not any categories'});
-		}
-	});
-
-	router.post('/add', async(req, res) => {
-		const title = req.body.title;
-
-		if (!title) {
-			return res.status(400).send({message: 'Please fill the gap - title'});
-		}
-
-    const isCategoryExist = await Category.findOne({title});
-    if (isCategoryExist) res.status(400).send({message: 'This category has already existed.'});
-
+  router.get('/list', async (req, res) => {
     try {
-      const category = new Category(req.body);
-      const newCategory = await category.save();
-      if (newCategory) res.send({message: "The Category added successfully."});
-    } catch (e) {
-      return res.status(400).send({message: 'Error. Unable to add category.'})
+      const categories = await Category.find().populate({path: 'parentId', select: 'title'});
+      if (categories) res.send(categories);
+    } catch (error) {
+      return res.status(404).send({ message: 'There are not any categories' });
     }
-	});
+  });
 
-	return router;
+  router.get('/parent/list', async (req, res) => {
+    try {
+      const categories = await Category.find({"parentId": null});
+      if (categories) res.send(categories);
+    } catch (error) {
+      return res.status(404).send({ message: 'There are not any parent categories' });
+    }
+  });
+
+  router.post('/add', async (req, res) => {
+    const request = req.body;
+
+    if (!request.title) {
+      return res.status(400).send({ message: 'Please fill the gap - title' });
+    }
+
+    const isCategoryExist = await Category.findOne({ title: request.title });
+    if (isCategoryExist) {
+      res.status(400).send({ message: 'This category has already existed.' });
+    } else {
+      try {
+        const category = new Category(request);
+        const newCategory = await category.save();
+        if (newCategory) res.send({ message: 'The Category added successfully.' });
+      } catch (e) {
+        return res.status(400).send({ message: 'Error. Unable to add new category.' });
+      }
+    }
+
+  });
+
+  return router;
 };
 
 module.exports = createRouter;
