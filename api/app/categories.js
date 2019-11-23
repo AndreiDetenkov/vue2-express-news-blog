3e2ae3bd990c078fb435a0feebeb058777077f32
AndreiDetenkov <a.detenkov@gmail.com>
2019-11-23 14:08:04 +0600

    # categories fixed code
    - added to responses error message;
    - changed error messages text;
    - changed check empty parameter;

--- api/app/categories.js
+++ api/app/categories.js
@@ -11,31 +11,27 @@ const createRouter = () => {
 				res.send(categories);
 			}
 		} catch (error) {
-			return res.status(500).send({message: 'Not found any categories'});
+			return res.status(500).send({message: 'There are not any categories'});
 		}
 	});
 
 	router.post('/add', async(req, res) => {
 		const title = req.body.title;
-		if (title === '') {
+
+		if (!title) {
 			return res.status(400).send({message: 'Please fill the gap - title'});
 		}
 
-		try {
-			const isCategoryExist = await Category.findOne({title});
-			if (isCategoryExist) res.status(400).send({error: 'This category has already existed.'});
-			else {
-				try {
-					const category = new Category(req.body);
-					const newCategory = await category.save();
-					if (newCategory) res.send({message: "The Category added successfully."});
-				} catch (e) {
-					return res.status(400).send({error: 'Error. Unable to add category.'})
-				}
-			}
-		} catch (e) {
-			return res.status(400).send({error: e});
-		}
+    const isCategoryExist = await Category.findOne({title});
+    if (isCategoryExist) res.status(400).send({message: 'This category has already existed.'});
+
+    try {
+      const category = new Category(req.body);
+      const newCategory = await category.save();
+      if (newCategory) res.send({message: "The Category added successfully."});
+    } catch (e) {
+      return res.status(400).send({message: 'Error. Unable to add category.'})
+    }
 	});
 
 	return router;
